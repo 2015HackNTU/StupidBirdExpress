d7fdc02385fc6a9b2e064dc5def7151752fe4560
Abby Yeh <radleaf@gmail.com>
2015-03-15 14:16:56 +0800

    authen

--- web/dart/authen.dart
+++ web/dart/authen.dart
@@ -1,8 +1,41 @@
 library client.authen;
 
 import 'dart:html';
-import 'package:StupidBirdExpress/client/email_send.dart';
+//import 'package:StupidBirdExpress/client/email_send.dart';
 
 void main() {
-  print('123');
+  String sequence;
+  InputElement receiverNameInput = querySelector('#receiverName');
+  InputElement receiverEmailInput = querySelector('#receiverName');
+  //is reply
+  if (!window.location.search.isEmpty) {
+    //get sequence
+    String search = window.location.search.substring(1);
+    int position = search.indexOf('=');
+    if (search.substring(0, position) == 'id') {
+      sequence = search.substring(position);
+    }
+    //set receiverName, receiverEmail(not alloewd to chage)
+    receiverNameInput.value = "";
+    
+  }
+  
+  ButtonElement btn = querySelector('.submit-btn');
+  btn.onClick.listen((_) {
+    String receiverName = receiverNameInput.value;
+    String receiverEmail = (querySelector('#receiverEmail') as InputElement).value;
+    String userName = receiverEmailInput.value;
+    String userEmail = (querySelector('#userEmail') as InputElement).value;
+    //determine massage type
+    InputElement massageInput= querySelector('.tab-content .active input');
+    String massage;
+    File massageFile;
+    switch(massageInput.type) {
+      case 'text':
+        break;
+      case 'file':
+        break;
+    }
+
+  });
 }
